6f52c36455ba58d42414600dc834ad540ca5bf7c
claudiacaicedo <claudia.caicedo@pucese.edu.ec>
2019-08-16 12:48:05 -0500

    Horario

--- src/components/createHorario.vue
+++ src/components/createHorario.vue
@@ -5,18 +5,42 @@
       <div class="row">
         <div class="col-md-6">
           <div class="form-group">
-            <label>laboratorio:</label>
-            <input type="text" class="form-control" v-model="lab.nombre">
+            <label>Laboratorio:</label>
+            <input type="text" class="form-control" v-model="horario.laboratorio">
           </div>
         </div>
         </div>
         <div class="row">
-          <div class="col-md-6">
-            <div class="form-group">
-              <label>Lab Descripcion:</label>
-              <input tyte="text" class="form-control" v-model="lab.descripcion">
-            </div>
+        <div class="col-md-6">
+          <div class="form-group">
+            <label>Docente:</label>
+            <input type="text" class="form-control" v-model="horario.docente">
           </div>
+        </div>
+        </div>
+        <div class="row">
+        <div class="col-md-6">
+          <div class="form-group">
+            <label>Materia:</label>
+            <input type="text" class="form-control" v-model="horario.materia">
+          </div>
+        </div>
+        </div>
+        <div class="row">
+        <div class="col-md-6">
+          <div class="form-group">
+            <label>Hora de Entrada:</label>
+            <input type="text" class="form-control" v-model="horario.horaEntrada" placeholder="hh:mm">
+          </div>
+        </div>
+        </div>
+        <div class="row">
+        <div class="col-md-6">
+          <div class="form-group">
+            <label>Hora de Salida:</label>
+            <input type="text" class="form-control" v-model="horario.horaSalida" placeholder="hh:mm">
+          </div>
+        </div>
         </div><br />
         <div class="form-group">
           <button class="btn btn-primary">Create</button>
@@ -29,14 +53,14 @@
     export default {
         data(){
         return {
-          lab:{}
+          horario:{}
         }
     },
     methods: {
       addHorario(){
-        let uri = 'http://localhost:3000/labs';
-        this.axios.post(uri, this.lab).then(() => {
-        this.$router.push({name: 'labs'});
+        let uri = 'http://localhost:3000/horarios';
+        this.axios.post(uri, this.horario).then(() => {
+        this.$router.push({name: 'horarios'});
     });
       }
     }
--- src/components/horario.vue
+++ src/components/horario.vue
@@ -4,24 +4,29 @@
         <div class="row">
           <div class="col-md-10"></div>
           <div class="col-md-2">
-            <router-link :to="{ name: 'createl' }" class="btn btn-primary">Crear Lab</router-link>
+            <router-link :to="{ name: 'createh' }" class="btn btn-primary">Crear Horario</router-link>
           </div>
         </div><br />
 
         <table class="table table-hover">
             <thead>
             <tr>
-              <th>Nombre</th>
-              <th>Descripcion</th>
-              <th>Actions</th>
+              <th>Laboratorios</th>
+              <th>Docente</th>
+              <th>Materia</th>
+              <th>hora Entrada</th>
+              <th>Hora Salida</th>
             </tr>
             </thead>
             <tbody>
-                <tr v-for="lab in labs" :key="lab._id">
-                  <td>{{ lab.nombre }}</td>
-                  <td>{{ lab.descripcion }}</td>
-                  <td><router-link :to="{name: 'editl', params: { id: lab._id }}" class="btn btn-primary">Edit</router-link></td>
-                  <td><button class="btn btn-danger" @click.prevent="deleteLab(lab._id)">Delete</button></td>
+                <tr v-for="horario in horarios" :key="horario._id">
+                  <td>{{ horario.laboratorio }}</td>
+                  <td>{{ horario.docente }}</td>
+                  <td>{{ horario.materia }}</td>
+                  <td>{{ horario.horaEntrada }}</td>
+                  <td>{{ horario.horaSalida }}</td>
+                  <td><router-link :to="{name: 'edith', params: { id: horario._id }}" class="btn btn-primary">Edit</router-link></td>
+                  <td><button class="btn btn-danger" @click.prevent="deleteLab(horario._id)">Delete</button></td>
                 </tr>
             </tbody>
         </table>
@@ -32,21 +37,21 @@
   export default {
       data() {
         return {
-          labs: []
+          horarios: []
         }
       },
       created() {
-      let uri = 'http://localhost:3000/labs';
+      let uri = 'http://localhost:3000/horarios';
       this.axios.get(uri).then(response => {
-        this.labs = response.data;
+        this.horarios = response.data;
       });
     },
     methods: {
       deleteLab(id)
       {
-        let uri = `http://localhost:3000/labs/${id}`;
+        let uri = `http://localhost:3000/horarios/${id}`;
         this.axios.delete(uri).then(response => {
-          this.labs.splice(this.labs.indexOf(id), 1);
+          this.horarios.splice(this.horarios.indexOf(id), 1);
         });
       }
     }
